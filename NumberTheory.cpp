d55ce86012c6ccd0475dd767ba380ff73dd71a9d
ABHISHEK BIND <abhishekbind2013@gmail.com>
2017-05-25 14:56:55 +0530

    Update NumberTheory.cpp

--- NumberTheory.cpp
+++ NumberTheory.cpp
@@ -35,3 +35,42 @@ ll modInverse(ll a,ll mod)
 {	
 	return modPower(a,mod-2,mod); 
 }
+// Factoring
+vector<pair<int,int> > factors;
+void getFactors(int n)
+{
+	factors.clear();
+	int temp;
+	int d = 1;
+	for(int i = 2;i*i<=n;i+=d,d=2)
+	{
+		if(n%i==0)
+		{
+			temp = 0;
+			while(n%i==0)
+			{
+				n/=i;
+				++temp;
+			}
+			factors.pb(mp(i,temp));
+		}
+	}
+	if(n!=1)
+		factors.pb(mp(n,1));
+}
+
+// Finding Divisors
+vector<int> divisors;
+void getDivisors(int ind = 0;int res = 1)
+{
+	if(ind == factors.size())
+	{
+		divisors.push_back(res);
+		return;
+	}
+	for (int i = 0; i <= factors[ind].second; i++) 
+	{
+        getDivisors(ind + 1, res);
+        res *= factors[ind].first;
+    }
+}
